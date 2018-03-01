55fc785119ed50936960be1d161a117817fcee97
rishabhjain7b <rishabhjain7b@gmail.com>
2018-03-01 15:32:54 +0530

    fourth commit

--- BRG.v
+++ BRG.v
@@ -3,8 +3,8 @@
 // divisor = (fclk/(BRmax*C_samples*2))
 //`define BRmax 38400
 
-`define C_samples 8
-`define divisor 3
+`define C_samples 4 //2**3=8
+`define divisor 2
 
 module BRG #(parameter sel=3'b000)
 (input fclk,
@@ -12,25 +12,29 @@ module BRG #(parameter sel=3'b000)
  output bclk,bclkx8);
 
 reg clkdiv;
-reg [7:0]bits;
-reg bclkx8_r,bclk_r;
+wire bclk_r;
+//reg [7:0]limit=2;
+//reg bclkx8_r,bclk_r;
+reg [1:0]q=2'd0;
+//reg [2:0]k=3'd0;
 
-assign bclkx8 = bclkx8_r,
-	bclk = bclk_r;
+counter_8bit #(.limit(8'd1<<(sel+1))) eight_bits_counter (.clkdiv(clkdiv),.out(bclkx8));
+
+counter_8bit #(.limit(`C_samples)) divide_by_C_reg (.clkdiv(bclkx8),.out(bclk_r));
+
+counter_8bit #(.limit(8'd1<<(sel+1))) divide_by_C (.clkdiv(bclk_r),.out(bclk));
 
 always @ (posedge fclk)
 begin
-	clkdiv = fclk/(`divisor);
-	case(sel)
-	3'b000: bclkx8_r = clkdiv >> 1;
-	3'b001: bclkx8_r = clkdiv >> 2;
-	3'b010: bclkx8_r = clkdiv >> 3;
-	3'b011: bclkx8_r = clkdiv >> 4;
-	3'b100: bclkx8_r = clkdiv >> 5;
-	3'b101: bclkx8_r = clkdiv >> 6;
-	3'b110: bclkx8_r = clkdiv >> 7;
-	3'b111: bclkx8_r = clkdiv >> 8;
-	endcase
-	bclk_r = bclkx8_r/`C_samples;
+	if (q == `divisor)
+	begin
+		q <= 2'd0;
+		clkdiv <= 1'b0;
+	end
+	else
+	begin
+		q <= q+1;
+		clkdiv <= 1'b1;
+	end
 end
 endmodule
